0d2082a9859f083a927222f2e40c6d9e3a7a48fe
rutagt09 <rutagt@gmail.com>
2017-12-03 04:34:35 -0800

    Remove Test Class

--- src/tester.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-
-public class tester {
-	public static void main(String args[]){
-		String x = new File("").getAbsolutePath();
-		x += "/testing/";
-		new File(x).mkdir();
-	}
-}
